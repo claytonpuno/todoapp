e9dae3f4ca7eb8a0da78cf83304c26bf451c4fd8
Clayton Puno <claytonpuno@gmail.com>
2020-09-30 04:34:37 -0400

    Updates

---
+++ precache-manifest.c3878f4dfeaba26fd935ef02500e8b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1aa43f0705fd50966cfcc665f5746db",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "1e516778ad68a451681b",
+    "url": "/todoapp/static/css/main.e150c203.chunk.css"
+  },
+  {
+    "revision": "cbc8ed23c00757d319e9",
+    "url": "/todoapp/static/js/2.b24cfec3.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/todoapp/static/js/2.b24cfec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e516778ad68a451681b",
+    "url": "/todoapp/static/js/main.f8562177.chunk.js"
+  },
+  {
+    "revision": "10a8c3fbc020010d9fe1",
+    "url": "/todoapp/static/js/runtime-main.7fd9679d.js"
+  }
+]);
